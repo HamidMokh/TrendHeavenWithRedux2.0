76eab60e04525c8476d3503c84bc2fa97c4b1e82
Hamid Mokhtari <mokhtarihamid44@gmail.com>
2025-01-10 17:27:58 +0100

    bug correction

--- src/store/categories/categories.saga.js
+++ src/store/categories/categories.saga.js
@@ -1,20 +1,13 @@
 import { takeLatest, all, call, put } from "redux-saga/effects";
-
 import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils"
-import { fetchCategoriesSucess, fetchCategoriesFailed } from "./categories.action";
+import { fetchCategoriesSuccess, fetchCategoriesFailed } from "./categories.action";
 import { CATEGORIES_ACTION_TYPES } from "./categories.types";
 
-
 export function* fetchCategoriesAsync(){
-
-    try {
-        const categoriesArray = yield call(getCategoriesAndDocuments,'categories'); 
-        yield put(fetchCategoriesSucess(categoriesArray));
-
-    }catch(error) {
-        yield put(fetchCategoriesFailed(error));
-    };
-
+try {
+const categoriesArray = yield call(getCategoriesAndDocuments,'categories'); 
+yield put(fetchCategoriesSuccess(categoriesArray));
+}catch(error) {yield put(fetchCategoriesFailed(error));};
 }
 
 export function* onFetchCategories(){
