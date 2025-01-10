3d6d259e7acb239144870bb42f6ad1b9786abafa
Hamid Mokhtari <mokhtarihamid44@gmail.com>
2025-01-10 16:24:59 +0100

    adding categories saga

--- src/store/store.js
+++ src/store/store.js
@@ -3,36 +3,41 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
-import { thunk } from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import { rootSaga } from './root-saga';
 
 import { rootReducer } from './root-reducer';
 
 // import { loggerMiddleware } from './middleware/logger';
 
+const sagaMiddleware = createSagaMiddleware();
+
 const middleWares = [
     process.env.NODE_ENV !=='production' && logger,
-    thunk,
+   sagaMiddleware,
 ].filter(Boolean); // a great way to avoid logger from appearing on production, i have chosed !== production instead of development because this is the safest option (in case i wanted to have other environments)
 
 const composedEnhancer = 
 (process.env.NODE_ENV !== 'production' &&
     window && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
     compose;
-
 const persistConfig = {
   key: 'root',
   storage,
   whitelist: ['cart'],
 };
 
+
+
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
+
 
 export const store = createStore(
   persistedReducer,
   undefined,
   composedEnhancers
 );
-
+sagaMiddleware.run(rootSaga);
 export const persistor = persistStore(store);
